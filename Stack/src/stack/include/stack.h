5a7eeceaef7ea6fdebfe75d4a91201fb0e58f9aa
Ajay K <76083469+ajaypep@users.noreply.github.com>
2022-09-30 20:47:58 +0530

    Add pop(),push(),peek(),print() for Stack

---
+++ Stack/src/stack/include/stack.h
@@ -0,0 +1,66 @@
+//
+// Created by ajay on 9/30/22.
+//
+
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
+
+#include <cstddef>
+#include<exception>
+#include <array>
+#include <iostream>
+
+template<typename T, std::size_t N>
+class Stack
+{
+    int m_top{-1};
+    std::size_t m_capacity{N};
+    std::array<T,N> m_data;
+    [[nodiscard]]bool is_empty()
+    {
+        return m_top==-1;
+    }
+    [[nodiscard]]bool is_full()
+    {
+        return m_top==(m_capacity-1);
+    }
+public:
+    void push(T ele)
+    {
+        if(is_full())
+        {
+            throw std::length_error("Stack is full. Stack overflow occurred.");
+        }
+        m_data[++m_top]=ele;
+    }
+    T pop()
+    {
+        if(is_empty())
+        {
+            throw std::length_error("Stack is empty. Stack underflow occurred.");
+        }
+        return m_data[m_top--];
+    }
+    T peek()
+    {
+        if(is_empty())
+        {
+            throw std::length_error("Stack is empty.");
+        }
+        return m_data[m_top];
+    }
+    void print()
+    {
+        if(is_empty())
+        {
+            throw std::length_error("Stack is empty.");
+        }
+        std::cout<<"Stack Top: "<<m_top<<" Capacity: "<<m_capacity<<'\n';
+        for(int i{m_top};i>=0;--i)
+        {
+            std::cout<<m_data[i]<<'\n';
+        }
+    }
+};
+
+#endif //STACK_STACK_H
