c7cf6ad0e878ed591007add48e1c7ec9a3402b97
Ajay K <76083469+ajaypep@users.noreply.github.com>
2022-10-20 14:24:26 +0530

    Rename Generic Tests to Other Tests.

--- StaticArray/tests/other_tests.cpp
+++ StaticArray/tests/other_tests.cpp
@@ -4,7 +4,7 @@
 #include "static_array.h"
 #include "gtest/gtest.h"
 
-TEST(Generic, NewArrayLenghtIsZero)
+TEST(OtherTests, NewArrayLenghtIsZero)
 {
     constexpr ds_impl::StaticArray<int,0> static_array{};
     ASSERT_EQ(static_array.get_length(),0);
