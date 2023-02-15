13edbca26698d78fb0021295c6324c9759e79235
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-02-15 17:58:33 +0530

    Add doubly linked list

--- LinkedList/sem1/doubly_list.cpp
+++ LinkedList/sem1/doubly_list.cpp
@@ -1,45 +1,44 @@
 #include <cassert>
 #include <iostream>
 
-class CircularList {
+class DoublyList {
   struct Node {
     int key;
     Node *next = nullptr;
+    Node *prev = nullptr;
     Node(int key) { this->key = key; }
   };
+  Node *m_head = nullptr;
   Node *m_tail = nullptr;
   Node *get_node_before(int index) {
-    Node *curr = m_tail->next;
+    Node *curr = m_head;
     int curr_index = 0;
-    do {
+    while (curr_index != index - 1) {
       curr = curr->next;
       ++curr_index;
-    } while (curr_index != index - 1);
+    }
     return curr;
   }
 
 public:
   bool is_empty() { return get_size() == 0; }
   int get_size() {
-    if (m_tail == nullptr) {
-      return 0;
-    }
-    Node *curr = m_tail->next;
+    Node *curr = m_head;
     int size = 0;
-    do {
+    while (curr != nullptr) {
       ++size;
       curr = curr->next;
-    } while (curr != m_tail->next);
+    }
     return size;
   }
   void insert_at_head(int key) {
     Node *new_node = new Node(key);
     if (get_size() == 0) {
-      m_tail = new_node;
-      m_tail->next = m_tail;
+      m_tail = m_head = new_node;
     } else {
-      new_node->next = m_tail->next;
-      m_tail->next = new_node;
+      new_node->next = m_head;
+      m_head->prev = new_node;
+      m_head = new_node;
     }
   }
   void insert_after_tail(int key) {
@@ -47,10 +46,9 @@ public:
     if (get_size() == 0) {
       insert_at_head(key);
     } else {
-      Node *head = m_tail->next;
+      new_node->prev = m_tail;
       m_tail->next = new_node;
       m_tail = m_tail->next;
-      m_tail->next = head;
     }
   }
   void insert_at(int index, int key) {
@@ -61,18 +59,22 @@ public:
     } else {
       Node *new_node = new Node(key);
       Node *target_node = get_node_before(index);
+      new_node->prev = target_node;
+      target_node->next->prev = new_node;
       new_node->next = target_node->next;
       target_node->next = new_node;
     }
   }
   void delete_head() {
     assert(get_size() != 0 && "Can't delete head, linked list is empty");
-    Node *deletion_node = m_tail->next;
+    Node *deletion_node = m_head;
+    Node *new_head = m_head->next;
     if (get_size() == 1) {
       m_tail = nullptr;
     } else {
-      m_tail->next = deletion_node->next;
+      new_head->prev = nullptr;
     }
+    m_head = new_head;
     delete deletion_node;
   }
   void delete_tail() {
@@ -80,10 +82,9 @@ public:
     if (get_size() == 1) {
       delete_head();
     } else {
-      Node *head = m_tail->next;
       Node *deletion_node = m_tail;
-      m_tail = get_node_before(get_size() - 1);
-      m_tail->next = head;
+      m_tail = m_tail->prev;
+      m_tail->next = nullptr;
       delete deletion_node;
     }
   }
@@ -97,50 +98,64 @@ public:
     } else {
       Node *target_node = get_node_before(index);
       Node *deletion_node = target_node->next;
+      deletion_node->next->prev = target_node;
       target_node->next = deletion_node->next;
       delete deletion_node;
     }
   }
   void display() {
-    std::cout << "Circular List(" << get_size() << ") : ";
+    std::cout << "Doubly List(" << get_size() << ") :-\n";
     if (get_size() == 0)
       return;
-    Node *curr = m_tail->next;
-    do {
-      std::cout << curr->key << " -> ";
+    std::cout << "\tOriginal: ";
+    Node *curr = m_head;
+    while (curr != nullptr) {
+      std::cout << curr->key;
+      if (curr->next == nullptr)
+        break;
+      std::cout << " -> ";
       curr = curr->next;
-    } while (curr != m_tail->next);
+    }
+    std::cout << "\n\tReverse: ";
+    curr = m_tail;
+    while (curr != nullptr) {
+      std::cout << curr->key;
+      if (curr->prev == nullptr)
+        break;
+      std::cout << " -> ";
+      curr = curr->prev;
+    }
     std::cout << '\n';
   }
   int search(int key) {
     int index = -1;
-    Node *curr = m_tail->next;
-    do {
+    Node *curr = m_head;
+    while (curr != nullptr) {
       ++index;
       if (curr->key == key) {
         return index;
       }
       curr = curr->next;
-    } while (curr != m_tail->next);
+    }
     return -1;
   }
   void reverse() {
-    Node *head = m_tail->next;
-    Node *prev = m_tail;
-    Node *curr = m_tail->next;
+    Node *curr = m_head;
+    Node *prev = nullptr;
     Node *next = nullptr;
-    do {
+    while (curr != nullptr) {
       next = curr->next;
       curr->next = prev;
       prev = curr;
       curr = next;
-    } while (curr != head);
-    m_tail = head;
+    }
+    m_tail = m_head;
+    m_head = prev;
   }
 };
 
-void test_circular_list() {
-  CircularList ll;
+void test_singly_list() {
+  DoublyList ll;
   ll.insert_after_tail(33);
   ll.insert_after_tail(3);
   ll.insert_after_tail(2);
@@ -171,6 +186,27 @@ void test_circular_list() {
 }
 
 int main() {
-  test_circular_list();
+  DoublyList ll;
+  ll.insert_after_tail(33);
+  ll.insert_after_tail(3);
+  ll.insert_after_tail(2);
+  ll.insert_after_tail(43);
+  ll.insert_at_head(99);
+  ll.insert_at(4, 32);
+  ll.display();
+  ll.delete_head();
+  std::cout << "After deleting head\n";
+  ll.display();
+  ll.delete_tail();
+  std::cout << "After deleting tail\n";
+  ll.display();
+  ll.delete_at(2);
+  std::cout << "After deleting node at index 2\n";
+  ll.display();
+  ll.insert_at(2, 98);
+  std::cout << "After inserting 98 at index 2\n";
+  ll.display();
+  int key = 98;
+  std::cout << "Key " << key << " is at index: " << ll.search(key) << '\n';
   return 0;
 }
