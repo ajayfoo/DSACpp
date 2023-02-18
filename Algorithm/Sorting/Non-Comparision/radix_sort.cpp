1df0004f5acb6ff01223b0636c588e7db58d08f6
Ajay K <76083469+ajaypep@users.noreply.github.com>
2023-02-18 14:20:31 +0530

    Add radix sort

--- Algorithm/Sorting/Non-Comparision/radix_sort.cpp
+++ Algorithm/Sorting/Non-Comparision/radix_sort.cpp
@@ -24,29 +24,38 @@ void print_array(int *arr, int n) {
   std::cout << '\n';
 }
 
-void counting_sort(int *arr, int n) {
-  int k = get_max(arr, n) + 1;
-  int *aux = new int[k]();
+int digit_at(int num, int place) { return (num / place) % 10; }
+
+void counting_sort(int *arr, int place, int n) {
+  constexpr int NUM_OF_DIGITS = 10;
+  int *aux = new int[NUM_OF_DIGITS]();
   int *sorted_arr = new int[n];
   for (int i = 0; i < n; ++i) {
-    ++aux[arr[i]];
+    ++aux[digit_at(arr[i], place)];
   }
-  for (int i = 1; i < k; ++i) {
+  for (int i = 1; i < NUM_OF_DIGITS; ++i) {
     aux[i] += aux[i - 1];
   }
   for (int i = n - 1; i >= 0; --i) {
-    sorted_arr[aux[arr[i]] - 1] = arr[i];
-    --aux[arr[i]];
+    sorted_arr[aux[digit_at(arr[i], place)] - 1] = arr[i];
+    --aux[digit_at(arr[i], place)];
   }
   copy_array(sorted_arr, arr, n);
 }
 
+void radix_sort(int *arr, int n) {
+  int max = get_max(arr, n);
+  for (int place = 1; max / place > 0; place *= 10) {
+    counting_sort(arr, place, n);
+  }
+}
+
 int main() {
   constexpr int SIZE = 4;
-  int arr[SIZE] = {7, 1, 0, 8};
+  int arr[SIZE] = {3, 1729, 891, 890};
   std::cout << "Before sorting:-\n";
   print_array(arr, SIZE);
-  counting_sort(arr, SIZE);
+  radix_sort(arr, SIZE);
   std::cout << "After sorting:-\n";
   print_array(arr, SIZE);
   return 0;
